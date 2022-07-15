a06aaaf44eab9024b58b8b733dfb46af6164f254
akkii03 <akkiindoria72@gmail.com>
2022-07-15 15:17:14 +0530

    doine

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,24 @@ import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+const [state,dispatch] = useReducer(counterReducer,{count:0});
+
+
+function increaseHandel () {
+  console.log("inc btn click")
+  dispatch({type:"add",payload:state.count})
+}
+
+function decreaseHandel () {
+  console.log("dec btn clicked");
+ 
+ 
+}
   return (
     <div id="main">
-
+        <button id='increment-btn' onClick={increaseHandel}>+</button>
+        <span id='counter'>{state.count}</span>
+        <button id='decrement-btn' onClick={decreaseHandel}>-</button>
     </div>
   )
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,14 @@
 const counterReducer = (state,action) =>{
+    switch(action.type) {
+        case"add": 
+        return console.log("inside reducer fun inc ",action.payload=state.count+1)
+        case"minus":
+        return console.log("inside the reducer fun dec",action.payload = state.count-1);
+        
+        default:
+            console.log("default case the state is ",state);
+            return state;
+    }
 
 }
 
