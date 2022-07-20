739da42e98fa8a74ca652c3068cf59f94f9b4f2c
akkii03 <akkiindoria72@gmail.com>
2022-07-19 16:26:16 -1200

    ques done

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,22 @@ import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{count:0});
+const [state,dispatch] = useReducer(counterReducer,0);
 
-
+console.log(state)
 function increaseHandel () {
-  console.log("inc btn click")
-  dispatch({type:"add",payload:state.count})
+  dispatch({type:"add",payload:state})
 }
 
 function decreaseHandel () {
-  console.log("dec btn clicked");
-  dispatch({type:"minus",payload:state.count});
+  dispatch({type:"minus",payload:state});
  
  
 }
   return (
     <div id="main">
         <button id='increment-btn' onClick={increaseHandel}>+</button>
-        <span id='counter'>{state.count}</span>
+        <span id='counter'>{state}</span>
         <button id='decrement-btn' onClick={decreaseHandel}>-</button>
     </div>
   )
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,12 +1,12 @@
 const counterReducer = (state,action) =>{
     switch(action.type) {
         case"add": 
-        console.log("inside reducer fun inc ",action.payload=state.count+1)
-        return state = action.payload-1;
+        console.log("inside reducer fun inc ",action.payload=state+1)
+        return state = action.payload=state+1;
 
         case"minus":
-        console.log("inside the reducer fun dec",action.payload = state.count-1);
-        return state = action.payload-1;
+        console.log("inside the reducer fun dec",action.payload = state-1);
+        return state = action.payload = state-1;
         
         default:
             console.log("default case the state is ",state);
